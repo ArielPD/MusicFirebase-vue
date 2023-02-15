89a06b5b4000b1a6b671a0686aef63b9693765c9
Ariel Pinto <arielpinto1041@gmail.com>
2023-02-14 23:03:48 -0400

    Update lista after created comment and add sort list comments

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -58,6 +58,7 @@
         </vee-form>
         <!-- Sort Comments -->
         <select
+          v-model="sort"
           class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
         >
           <option value="1">Latest</option>
@@ -70,7 +71,7 @@
   <ul class="container mx-auto">
     <li
       class="p-6 bg-gray-50 border border-gray-200"
-      v-for="comment in comments"
+      v-for="comment in sortedComments"
       :key="comment.docId"
     >
       <!-- Comment Author -->
@@ -115,10 +116,21 @@ export default {
       comment_variant: "bg-blue-500",
       comment_alert_message: "Please wait! Your comment is being ubmitted.",
       comments: [],
+      sort: '1',
     };
   },
   computed: {
     ...mapState(useUserStore, ["userLoggedIn"]),
+    sortedComments() {
+      return this.comments.slice().sort((a, b) => {
+        if(this.sort === '1') {
+          return new Date(b.datePosted) - new Date(a.datePosted);
+        } 
+
+        return new Date(a.datePosted) - new Date(b.datePosted);
+
+      });
+    },
   },
   async created() {
     //const docSnapshot = await songsCollection.doc(this.$route.params.id).get();
@@ -152,6 +164,8 @@ export default {
 
       //await commentsCollection.add(comment);
       await addDoc(commentsCollection, comment);
+
+      this.getComments();
 
       this.comment_in_submission = false;
       this.comment_variant = "bg-green-500";
