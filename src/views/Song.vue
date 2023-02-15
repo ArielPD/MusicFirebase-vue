3872e2e5b8429e5ab55fb20749c53613781f760b
Ariel Pinto <arielpinto1041@gmail.com>
2023-02-15 11:13:14 -0400

    Storing the song in the stage of store

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -10,6 +10,7 @@
       <button
         type="button"
         class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
+        @click.prevent="newSong(song)"
       >
         <i class="fas fa-play"></i>
       </button>
@@ -101,8 +102,9 @@ import {
   onSnapshot,
   updateDoc,
 } from "@/includes/firebase";
-import { mapState } from "pinia";
+import { mapState, mapActions } from "pinia";
 import useUserStore from "@/stores/user";
+import usePlayerStore from "@/stores/player";
 
 export default {
   name: "Song",
@@ -150,6 +152,7 @@ export default {
     this.getComments();
   },
   methods: {
+    ...mapActions(usePlayerStore, ["newSong"]),
     async addComment(values, { resetForm }) {
       this.comment_in_submission = true;
       this.comment_show_alert = true;
