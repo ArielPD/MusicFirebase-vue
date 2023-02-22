1364ea62c2458b00fd270a101083b73893d00e34
Ariel Pinto <arielpinto1041@gmail.com>
2023-02-22 12:16:36 -0400

    Offline Support with service worker

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/base.css';
-import './assets/main.css';
+import "./assets/base.css";
+import "./assets/main.css";
 import VeeValidatePlugin from "./includes/validation";
 import { auth } from "./includes/firebase";
 import Icon from "./directives/icon";
 import i18n from "./includes/i18n";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({ immediate: true });
 
 let app;
 auth.onAuthStateChanged(() => {
