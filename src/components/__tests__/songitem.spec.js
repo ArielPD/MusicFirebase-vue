37a610f7d7ed57f86ff61c8f592bc18ef14b4a1f
Ariel Pinto <arielpinto1041@gmail.com>
2023-02-27 09:58:10 -0400

    Testing attributes id in songItem

--- src/components/__tests__/songitem.spec.js
+++ src/components/__tests__/songitem.spec.js
@@ -19,7 +19,26 @@ describe("SongItem.vue", () => {
 
     //expect(wrapper.text()).toContain(song.display_name);
     //other away to test
-    const compositionAuthor = wrapper.find('.text-gray-500');
+    const compositionAuthor = wrapper.find(".text-gray-500");
     expect(compositionAuthor.text()).toBe(song.display_name);
+  });
+
+  test("renders song.docId in id attributes", () => {
+    const song = {
+      docId: "abc",
+    };
+    const wrapper = shallowMount(SongItem, {
+      propsData: {
+        song,
+      },
+      global: {
+        components: {
+          "router-link": RouterLinkStub,
+        },
+      },
+    });
+
+    expect(wrapper.attributes().id).toBe(`song-id-${song.docId}`);
+    //expect(wrapper.classes()).toContain(`song-id=${song.docId}`);
   });
 });
