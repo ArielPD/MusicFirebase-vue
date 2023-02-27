526a409a55407a3868473993359eec3a0e664459
Ariel Pinto <arielpinto1041@gmail.com>
2023-02-27 11:39:22 -0400

    Testing router components

--- src/components/SongItem.vue
+++ src/components/SongItem.vue
@@ -4,7 +4,10 @@
     :id="`song-id-${song.docId}`"
   >
     <div>
-      <router-link :to="{ name: 'song', params: {id: song.docId } }" class="font-bold block text-gray-600">
+      <router-link
+        :to="{ name: 'song', params: { id: song.docId } }"
+        class="font-bold block text-gray-600"
+      >
         {{ song.modified_name }}
       </router-link>
       <span class="text-gray-500 text-sm">
@@ -13,9 +16,11 @@
     </div>
 
     <div class="text-gray-600 text-lg">
-      <router-link  custom 
-          :to="{ name: 'song', params: {id: song.docId}, hash: '#comments' }"
-          v-slot="{ navigate }">
+      <router-link
+        custom
+        :to="{ name: 'song', params: { id: song.docId }, hash: '#comments' }"
+        v-slot="{ navigate }"
+      >
         <span class="comments" @click="navigate">
           <i class="fa fa-comments text-gray-600"></i>
           {{ song.comment_count }}
